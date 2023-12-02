a19a509aacde8983589270379ead106ac708a4b2
frhn16 <F.Mirza3@newcastle.ac.uk>
2023-12-02 10:11:06 +0000

    Game can now be replayed and separate condition for when you run out of cards

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -7,153 +7,180 @@ namespace CardGames
     {
         static void Main(string[] args)
         {
-            int noPlayers = 0;
-            string cardChoice;
-            int cardPosition;
-            string winner = "";
-            Stack<Card> cardDeckStack = new Stack<Card>();
-            List<Card> cardDeck = new List<Card>();
-            Stack<Card> cardPile = new Stack<Card>();
-            Random rand = new Random();
-            List<Player> playerList = new List<Player>();
-            bool playersChosen = false;
-            bool winnerFound = false;
-            bool gameOver = false;
-            bool cardPlaced = false;
-
-            for (int x = 0; x < 8; x++)
+            bool playAgain = false;
+            do
             {
-                for (int y = 0; y < 10; y++)
+                int noPlayers = 0;
+                string cardChoice;
+                int cardPosition;
+                string winner = "";
+                Stack<Card> cardDeckStack = new Stack<Card>();
+                List<Card> cardDeck = new List<Card>();
+                Stack<Card> cardPile = new Stack<Card>();
+                Random rand = new Random();
+                List<Player> playerList = new List<Player>();
+                bool playersChosen = false;
+                bool winnerFound = false;
+                bool cardPlaced = false;
+
+                for (int x = 0; x < 8; x++)
                 {
-                    switch (x)
+                    for (int y = 0; y < 10; y++)
                     {
-                        case 0:
-                            cardDeck.Add(new Card(y, "Red", "number"));
-                            break;
-                        case 1:
-                            if (y != 0)
+                        switch (x)
+                        {
+                            case 0:
                                 cardDeck.Add(new Card(y, "Red", "number"));
-                            break;
-                        case 2:
-                            cardDeck.Add(new Card(y, "Yellow", "number"));
-                            break;
-                        case 3:
-                            if (y != 0)
+                                break;
+                            case 1:
+                                if (y != 0)
+                                    cardDeck.Add(new Card(y, "Red", "number"));
+                                break;
+                            case 2:
                                 cardDeck.Add(new Card(y, "Yellow", "number"));
-                            break;
-                        case 4:
-                            cardDeck.Add(new Card(y, "Green", "number"));
-                            break;
-                        case 5:
-                            if (y != 0)
+                                break;
+                            case 3:
+                                if (y != 0)
+                                    cardDeck.Add(new Card(y, "Yellow", "number"));
+                                break;
+                            case 4:
                                 cardDeck.Add(new Card(y, "Green", "number"));
-                            break;
-                        case 6:
-                            cardDeck.Add(new Card(y, "Blue", "number"));
-                            break;
-                        case 7:
-                            if (y != 0)
+                                break;
+                            case 5:
+                                if (y != 0)
+                                    cardDeck.Add(new Card(y, "Green", "number"));
+                                break;
+                            case 6:
                                 cardDeck.Add(new Card(y, "Blue", "number"));
-                            break;
+                                break;
+                            case 7:
+                                if (y != 0)
+                                    cardDeck.Add(new Card(y, "Blue", "number"));
+                                break;
+                        }
                     }
                 }
-            }
-            // Randomises the cards
-            for (int i = cardDeck.Count - 1; i > 0; i--)
-            {
-                int k = rand.Next(i + 1);
-                Card value = cardDeck[k];
-                cardDeck[k] = cardDeck[i];
-                cardDeck[i] = value;
-            }
-            foreach (Card card in cardDeck)
-            {
-                cardDeckStack.Push(card);
-            }
-            do
-            {
-                Console.WriteLine("How many people are playing Uno?");
-                try
+                // Randomises the cards
+                for (int i = cardDeck.Count - 1; i > 0; i--)
                 {
-                    noPlayers = Convert.ToInt32(Console.ReadLine());
-                    for (int x = 1; x <= noPlayers; x++)
+                    int k = rand.Next(i + 1);
+                    Card value = cardDeck[k];
+                    cardDeck[k] = cardDeck[i];
+                    cardDeck[i] = value;
+                }
+                foreach (Card card in cardDeck)
+                {
+                    cardDeckStack.Push(card);
+                }
+                do
+                {
+                    Console.WriteLine("How many people are playing Uno?");
+                    try
                     {
-                        Console.WriteLine($"Enter a name for Player {x}");
-                        List<Card> hand = new List<Card>();
-                        for (int y=0; y<7; y++)
+                        noPlayers = Convert.ToInt32(Console.ReadLine());
+                        for (int x = 1; x <= noPlayers; x++)
                         {
-                            hand.Add(cardDeckStack.Pop());
+                            Console.WriteLine($"Enter a name for Player {x}");
+                            List<Card> hand = new List<Card>();
+                            for (int y = 0; y < 7; y++)
+                            {
+                                hand.Add(cardDeckStack.Pop());
+                            }
+                            playerList.Add(new Player(Console.ReadLine(), x, hand));
                         }
-                        playerList.Add(new Player(Console.ReadLine(), x, hand));
+                        playersChosen = true;
                     }
-                    playersChosen = true;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Please enter a number.");
-                }
-            } while (!playersChosen);
-
-            // Starting card
-            cardPile.Push(cardDeckStack.Pop());
-            do
-            {
-                for (int x=0; x<noPlayers; x++)
-                {
-                    do
+                    catch (FormatException)
                     {
-                        cardPlaced = false;
-                        Console.WriteLine($"Current Card on Pile: {cardPile.Peek().CardColour} {cardPile.Peek().CardVal}");
-                        Console.WriteLine($"{playerList[x].Username}'s Turn");
+                        Console.WriteLine("Please enter a number.");
+                    }
+                } while (!playersChosen);
 
-                        for (int y = 0; y < playerList[x].CardHand.Count; y++)
+                // Starting card
+                cardPile.Push(cardDeckStack.Pop());
+                do
+                {
+                    for (int x = 0; x < noPlayers; x++)
+                    {
+                        do
                         {
-                            Console.WriteLine($"Card {y + 1}: {playerList[x].CardHand[y].CardColour} {playerList[x].CardHand[y].CardVal},");
-                        }
-                        Console.WriteLine("\nSelect a card by its number position in your hand to place on the pile, or type 'draw' to draw a card and move on.");
-                        cardChoice = Console.ReadLine();
-                        if (cardChoice.ToUpper() == "DRAW")
-                        {
-                            playerList[x].AddCard(cardDeckStack.Pop());
-                            cardPlaced = true;
-                        }
-                        else
-                        {
-                            try
+                            cardPlaced = false;
+                            Console.WriteLine($"Current Card on Pile: {cardPile.Peek().CardColour} {cardPile.Peek().CardVal}");
+                            Console.WriteLine($"Cards left in Draw Deck: {cardDeckStack.Count}");
+                            Console.WriteLine($"{playerList[x].Username}'s Turn");
+
+                            for (int y = 0; y < playerList[x].CardHand.Count; y++)
                             {
-                                cardPosition = Convert.ToInt32(cardChoice);
-                                if (playerList[x].CardHand[cardPosition - 1].CardColour == cardPile.Peek().CardColour || playerList[x].CardHand[cardPosition - 1].CardVal == cardPile.Peek().CardVal)
+                                Console.WriteLine($"Card {y + 1}: {playerList[x].CardHand[y].CardColour} {playerList[x].CardHand[y].CardVal},");
+                            }
+                            Console.WriteLine("\nSelect a card by its number position in your hand to place on the pile, or type 'draw' to draw a card and move on.");
+                            cardChoice = Console.ReadLine();
+                            if (cardChoice.ToUpper() == "DRAW")
+                            {
+                                playerList[x].AddCard(cardDeckStack.Pop());
+                                cardPlaced = true;
+
+                                Console.WriteLine("New Hand:");
+                                for (int y = 0; y < playerList[x].CardHand.Count; y++)
                                 {
-                                    cardPlaced = true;
-                                    cardPile.Push(playerList[x].CardHand[cardPosition - 1]);
-                                    playerList[x].RemoveCard(cardPosition - 1);
+                                    Console.WriteLine($"Card {y + 1}: {playerList[x].CardHand[y].CardColour} {playerList[x].CardHand[y].CardVal},");
                                 }
-                                else
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                try
                                 {
-                                    Console.WriteLine("That card can't go on the pile.");
+                                    cardPosition = Convert.ToInt32(cardChoice);
+                                    if (playerList[x].CardHand[cardPosition - 1].CardColour == cardPile.Peek().CardColour || playerList[x].CardHand[cardPosition - 1].CardVal == cardPile.Peek().CardVal)
+                                    {
+                                        cardPlaced = true;
+                                        cardPile.Push(playerList[x].CardHand[cardPosition - 1]);
+                                        playerList[x].RemoveCard(cardPosition - 1);
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("That card can't go on the pile.\n");
+                                    }
+                                }
+                                catch (FormatException)
+                                {
+                                    Console.WriteLine("You didn't enter the number position of the card in your hand.\n");
+                                }
+                                catch (ArgumentOutOfRangeException)
+                                {
+                                    Console.WriteLine("Position entered is out of the range of your hand.\n");
                                 }
                             }
-                            catch (FormatException)
+                            if (playerList[x].CardHand.Count <= 0)
                             {
-                                Console.WriteLine("You didn't enter the number position of the card in your hand.");
+                                winnerFound = true;
+                                winner = playerList[x].Username;
+                                break;
                             }
-                            catch (ArgumentOutOfRangeException)
+                            else
                             {
-                                Console.WriteLine("Position entered is out of the range of your hand.");
+                                winnerFound = false;
+                                break;
                             }
-                        }
-                        if (playerList[x].CardHand.Count <= 0)
-                        {
-                            winnerFound = true;
-                            winner = playerList[x].Username;
+                        } while (!cardPlaced);
+                        if (winnerFound || cardDeck.Count <= 0)
                             break;
-                        }
-                    } while (!cardPlaced);
-                    if (winnerFound || cardDeck.Count <= 0)
-                        break;
-                }
-            } while (!winnerFound && cardDeck.Count > 0);
-            Console.WriteLine($"Winner is : {winner}!");
+                    }
+                } while (!winnerFound && cardDeck.Count > 0);
+                if (winnerFound)
+                    Console.WriteLine($"Winner is : {winner}!");
+                else
+                    Console.WriteLine("Ran out of cards from the draw deck, no one wins!");
+
+                Console.WriteLine("Would you like to play again?");
+                if (Console.ReadLine().ToUpper() == "YES" || Console.ReadLine().ToUpper() == "Y")
+                    playAgain = true;
+                else playAgain = false;       
+            } while (playAgain);
+
+            Console.WriteLine("Thanks for playing Uno, bye!");
             Console.ReadKey();
         }
     }
